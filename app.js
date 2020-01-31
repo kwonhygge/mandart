0afd9e1b8e007a55b71a8eb392eb1e41169167c8
kwonhygge <kitteniso1103@gmail.com>
2020-01-31 20:58:35 +0900

    Add signup&login page

--- app.js
+++ app.js
@@ -10,3 +10,23 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
+
+app.get("/signup",function(req,res){
+    res.render("signup");
+})
+
+app.get("/main",function(req,res){
+    res.render("main");
+})
+
+app.listen(3000,function(){
+    console.log("listening on port 3000");
+})
