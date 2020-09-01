0292d6281b0a1d404f3066d7c774a4241f081db1
kwonhygge <kitteniso1103@gmail.com>
2020-09-01 23:52:22 +0900

    Make theme color div to radio input. And Added event for clicking color. When color is clicked, box-background color will be changed.

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -1,10 +1,24 @@
-const input = document.querySelector('.obj-create');
-const SHOWING = "showing";
-const nextBtn = document.querySelector(".next-btn");
+const createBox = document.querySelector('.create-box');
+const themeBtns = document
+  .querySelector('.theme-colors')
+  .getElementsByTagName('input');
+const colors = ['green', 'pink', 'blue', 'olive', 'purple', 'dark'];
 
+function handleThemeColor(target) {
+  createBox.classList.add(target.id);
+  for (let i = 0; i < colors.length; i++) {
+    if (colors[i] !== target.id) {
+      createBox.classList.remove(colors[i]);
+    }
+  }
+}
 
-function init(){
-
+function init() {
+  for (let i = 0; i < themeBtns.length; i++) {
+    themeBtns[i].addEventListener('click', function (event) {
+      handleThemeColor(event.target);
+    });
+  }
 }
 
 init();
