283c0997a14234731f03e4bbd5cf6deb50d58f13
kwonhygge <kitteniso1103@gmail.com>
2020-09-04 17:56:44 +0900

    Prepare Heroku app

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ require('dotenv').config();
 //mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/objectiveDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  `mongodb+srv://admin-dory:${process.env.MONGO_PASS}@cluster0.b1gte.mongodb.net/objectiveDB`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -28,18 +31,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-//변수들
-let themeColor = 'white';
-let userId = '';
-
 //model 생성
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-//passport
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 const findOrCreate = require('mongoose-findorcreate');
 //bcrypt
@@ -61,93 +56,37 @@ app.use(
     saveUninitialized: false,
   })
 );
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-userSchema.plugin(passportLocalMongoose, { usernameField: 'username' });
-userSchema.plugin(findOrCreate);
-
 const User = new mongoose.model('User', userSchema);
-passport.use(User.createStrategy());
-
-passport.serializeUser(function (user, done) {
-  userId = user.id;
-  done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/mandart',
-      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-    }
-  )
-);
 
 app.get('/', function (req, res) {
   res.render('home');
 });
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile'] })
-);
-
-app.get(
-  '/auth/google/mandart',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function (req, res) {
-    res.redirect('/main');
-  }
-);
 
 app.get('/login', function (req, res) {
   res.render('login');
 });
 
 app.post('/login', function (req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
+  const username = req.body.username;
+  const password = req.body.password;
 
-  User.findOne({ email: user.username }, function (err, foundUser) {
+  User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
+      console.log('err');
       console.log(err);
     } else {
+      console.log('not err');
       if (foundUser) {
-        bcrypt.compare(user.password, foundUser.password, function (
-          err,
-          result
-        ) {
-          if (result === true) {
+        console.log('found');
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (err) {
+            console.log(err);
+          } else if (result === true) {
             res.redirect('/main');
           }
         });
       } else {
-        req.login(user, function (err) {
-          if (err) {
-            console.log(err);
-          } else {
-            passport.authenticate('local')(req, res, function () {
-              res.redirect('/main');
-            });
-          }
-        });
+        res.redirect('/signup');
       }
     }
   });
@@ -163,7 +102,6 @@ app.post('/signup', function (req, res) {
       email: req.body.username,
       password: hash,
     });
-    console.log(newUser);
 
     newUser.save(function (err) {
       if (err) {
@@ -193,41 +131,10 @@ app.get('/mainbox', function (req, res) {
 });
 
 app.post('/mainbox', function (req, res) {
-  // const mainBox_values = {
-  //   0: req.body.TopLeft,
-  //   1: req.body.Top,
-  //   2: req.body.TopRight,
-  //   3: req.body.Left,
-  //   mainObjective: req.body.mainObjective,
-  //   4: req.body.Right,
-  //   5: req.body.BottomLeft,
-  //   6: req.body.Bottom,
-  //   7: req.body.BottomRight,
-  // };
-  // if (buttonName === 'Save') {
-  //   console.log(mainBox);
-  //   User.updateOne(
-  //     { id: userId },
-  //     {
-  //       mainBox: mainBox,
-  //     },
-  //     function (err) {
-  //       if (err) {
-  //         console.log(err);
-  //       } else {
-  //         console.log('Successfully updated All');
-  //       }
-  //     }
-  //   );
-  //   res.redirect('/main');
-  // } else {
-  //   smallBoxIndex = buttonName;
-  //   smallBox.objective = mainBox_values[buttonName];
-  //   mainBox.plans[smallBoxIndex].objective = smallBox.objective;
-  //   res.redirect('/smallbox');
-  // }
+  console.log(req.body);
 });
 
 app.listen(process.env.PORT || 3000, function () {
+  console.log(process.env.TEST);
   console.log('listening on port 3000');
 });
