0ec6f67facc9700bd3787da882711008ed7e90b7
kwonhygge <kitteniso1103@gmail.com>
2020-09-04 17:28:03 +0900

    Cleaned project for being hosted on Heroku

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 //jshint esversion:6
 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 //dotenv
 require('dotenv').config();
+
 //mongoose
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  `mongodb+srv://admin-dory:${process.env.MONGO_PASSWORD}@cluster0.b1gte.mongodb.net/objectiveDB`,
+  `mongodb+srv://admin-dory:${process.env.MONGO_PASS}@cluster0.b1gte.mongodb.net/objectiveDB`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -14,7 +26,9 @@ mongoose.connect(
     useFindAndModify: false,
   }
 );
+const findOrCreate = require('mongoose-findorcreate');
 
+//model 생성
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
@@ -30,31 +44,11 @@ const userSchema = new mongoose.Schema({
     ],
   },
 });
+const User = new mongoose.model('User', userSchema);
 
-//변수들
-let themeColor = 'white';
-let userId = '';
-
-//model 생성
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-//passport
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const session = require('express-session');
-const findOrCreate = require('mongoose-findorcreate');
 //bcrypt
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 //session
 app.use(
@@ -65,92 +59,33 @@ app.use(
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-userSchema.plugin(passportLocalMongoose, { usernameField: 'username' });
-userSchema.plugin(findOrCreate);
-
-const User = new mongoose.model('User', userSchema);
-passport.use(User.createStrategy());
-
-passport.serializeUser(function (user, done) {
-  userId = user.id;
-  done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/mandart',
-      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-    }
-  )
-);
-
 app.get('/', function (req, res) {
   res.render('home');
 });
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile'] })
-);
-
-app.get(
-  '/auth/google/mandart',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function (req, res) {
-    res.redirect('/main');
-  }
-);
 
 app.get('/login', function (req, res) {
   res.render('login');
 });
 
 app.post('/login', function (req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
+  const username = req.body.username;
+  const password = req.body.password;
 
-  User.findOne({ email: user.username }, function (err, foundUser) {
+  User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
+      console.log('err');
       console.log(err);
     } else {
+      console.log('not err');
       if (foundUser) {
-        bcrypt.compare(user.password, foundUser.password, function (
-          err,
-          result
-        ) {
-          if (result === true) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (err) {
+          } else if (result === true) {
             res.redirect('/main');
           }
         });
       } else {
-        req.login(user, function (err) {
-          if (err) {
-            console.log(err);
-          } else {
-            passport.authenticate('local')(req, res, function () {
-              res.redirect('/main');
-            });
-          }
-        });
+        res.redirect('/signup');
       }
     }
   });
@@ -166,7 +101,6 @@ app.post('/signup', function (req, res) {
       email: req.body.username,
       password: hash,
     });
-    console.log(newUser);
 
     newUser.save(function (err) {
       if (err) {
@@ -197,39 +131,6 @@ app.get('/mainbox', function (req, res) {
 
 app.post('/mainbox', function (req, res) {
   console.log(req.body);
-  // const mainBox_values = {
-  //   0: req.body.TopLeft,
-  //   1: req.body.Top,
-  //   2: req.body.TopRight,
-  //   3: req.body.Left,
-  //   mainObjective: req.body.mainObjective,
-  //   4: req.body.Right,
-  //   5: req.body.BottomLeft,
-  //   6: req.body.Bottom,
-  //   7: req.body.BottomRight,
-  // };
-  // if (buttonName === 'Save') {
-  //   console.log(mainBox);
-  //   User.updateOne(
-  //     { id: userId },
-  //     {
-  //       mainBox: mainBox,
-  //     },
-  //     function (err) {
-  //       if (err) {
-  //         console.log(err);
-  //       } else {
-  //         console.log('Successfully updated All');
-  //       }
-  //     }
-  //   );
-  //   res.redirect('/main');
-  // } else {
-  //   smallBoxIndex = buttonName;
-  //   smallBox.objective = mainBox_values[buttonName];
-  //   mainBox.plans[smallBoxIndex].objective = smallBox.objective;
-  //   res.redirect('/smallbox');
-  // }
 });
 
 app.listen(process.env.PORT || 3000, function () {
