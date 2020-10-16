28ae86468418a02e5fe7ad181238c7d2c6c98f38
kwonhygge <kitteniso1103@gmail.com>
2020-10-16 20:39:55 +0900

    Allocates sign up messages

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 module.exports = function (passport, User) {
   const express = require('express');
+  const bcrypt = require('bcryptjs');
   const router = express.Router();
 
   router.get('/login', function (req, res) {
@@ -39,17 +40,55 @@ module.exports = function (passport, User) {
     }
   });
 
-  router.post('/signup', function (req, res, next) {
-    User.register(
-      new User({ username: req.body.username }),
-      req.body.password,
-      function (err) {
-        if (err) {
-          return next(err);
+  router.post('/signup', function (req, res) {
+    const { email, password, password2 } = req.body;
+    let erros = [];
+
+    if (!email || !password || !password2) {
+      erros.push({ msg: 'Please Enter all fields' });
+    }
+    if (password != password2) {
+      errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (password.length < 6) {
+      errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+
+    if (errors.length > 0) {
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2,
+      });
+    } else {
+      User.findOne({ email: email }).then((user) => {
+        if (user) {
+          error.push({ msg: 'Email already exists' });
+          res.render('signup', { errors, email, password, password2 });
+        } else {
+          const newUser = newUser({ name, email, password });
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser
+                .save()
+                .then((user) => {
+                  req.flash(
+                    'success_msg',
+                    'You are not registered and can log in'
+                  );
+                  res.redirect('/auth/login');
+                })
+                .catch((err) => console.log(err));
+            });
+          });
         }
-        res.redirect('/main');
-      }
-    );
+      });
+    }
   });
 
   return router;
