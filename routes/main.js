b4eae91e258e18f5c2210d23fcc75bcf0c988e38
kwonhygge <kitteniso1103@gmail.com>
2020-10-21 00:03:57 +0900

    Fixed bugs that values not appear

--- routes/main.js
+++ routes/main.js
@@ -12,7 +12,6 @@ module.exports = function (ps, User, Box) {
   router.get('/list/:id', ensureAuthenticated, async function (req, res) {
     const box = await Box.findById(req.params.id);
     if (box == null) res.redirect('/main');
-    console.log(box);
     res.render('show', { box, mode: "edit" });
   });
 
@@ -35,35 +34,35 @@ module.exports = function (ps, User, Box) {
       createdBy: req.user.id,
       smallPlans: [
         {
-          objective: req.body.smallObj[0][0],
+          objective: req.body.smallObj0[0],
           plans: req.body.plan0,
         },
         {
-          objective: req.body.smallObj[1][0],
+          objective: req.body.smallObj1[0],
           plans: req.body.plan1,
         },
         {
-          objective: req.body.smallObj[2][0],
+          objective: req.body.smallObj2[0],
           plans: req.body.plan2,
         },
         {
-          objective: req.body.smallObj[3][0],
+          objective: req.body.smallObj3[0],
           plans: req.body.plan3,
         },
         {
-          objective: req.body.smallObj[4][0],
+          objective: req.body.smallObj4[0],
           plans: req.body.plan4,
         },
         {
-          objective: req.body.smallObj[5][0],
+          objective: req.body.smallObj5[0],
           plans: req.body.plan5,
         },
         {
-          objective: req.body.smallObj[6][0],
+          objective: req.body.smallObj6[0],
           plans: req.body.plan6,
         },
         {
-          objective: req.body.smallObj[7][0],
+          objective: req.body.smallObj7[0],
           plans: req.body.plan7,
         },
       ],
