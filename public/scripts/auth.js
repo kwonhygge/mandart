a58c40a27f817cdd8afd56edaabcb19073336535
kwonhygge <kitteniso1103@gmail.com>
2020-10-22 18:04:59 +0900

    Checking password function for checking password length too

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,17 +1,37 @@
-const pass1 = document.getElementById('password1');
+const pass1 = document.getElementById('password');
 const pass2 = document.getElementById('password2');
-const circles = document.querySelectorAll('.confirm-circle');
+const circle1 = document.querySelector('#password~.confirm-circle');
+const circle2 = document.querySelector('#password2~.confirm-circle');
+const circles = document.querySelectorAll(".confirm-circle");
 
 const checkPassword = () => {
-  if (pass1.value === pass2.value) {
-    circles.forEach((circle) => (circle.style.background = 'green'));
-  } else {
-    circles.forEach((circle) => (circle.style.background = 'red'));
+  const isLengthProper = checkLength();
+  if (isLengthProper && pass2.value.length > 0) {
+    if (pass1.value === pass2.value) {
+      circle2.style.background = '#89A390';
+    } else {
+      circle1.style.background = '#FF0000';
+      circle2.style.background = '#FF0000';
+    }
   }
+
 };
+
+const checkLength = () => {
+  if (pass1.value.length >= 6) {
+    circle1.style.background = '#89A390'
+    return true;
+  } else {
+    circle1.style.background = '#FF0000'
+    return false;
+  }
+
+}
 
 const init = () => {
   pass1.addEventListener('change', checkPassword);
+  pass1.addEventListener('keyup', checkPassword);
   pass2.addEventListener('change', checkPassword);
+  pass2.addEventListener('keyup', checkPassword);
 };
 init();
