f77568c6a9a46ee3480278cc54534f9cb11e59a5
kwonhygge <kitteniso1103@gmail.com>
2020-10-24 00:17:28 +0900

    Adds initchecked method for setting checked for theme option input

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,9 +1,22 @@
 const themeOptions = document.querySelectorAll(".option input");
 
-themeOptions.forEach(option => {
-    option.addEventListener("click", function (e) {
-        document.documentElement.setAttribute("data-theme", e.target.value);
+const initChecked = () => {
+    themeId = sessionStorage.getItem("themeId");
+    themeOptions.forEach(option => {
+        if (option.value === themeId) {
+            option.checked = true;
+        }
     })
 }
 
-)
+const showInit = () => {
+    initChecked();
+    themeOptions.forEach(option => {
+        option.addEventListener("click", function (e) {
+            document.documentElement.setAttribute("data-theme", e.target.value);
+        })
+    }
+    )
+}
+
+showInit();
